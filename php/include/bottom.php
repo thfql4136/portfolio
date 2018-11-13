f7d560cc6797d6f2f42c23ccc4a00e281339547b
thfql4136 <thfql4136@naver.com>
2018-11-13 11:50:05 +0900

    181113-1

---
+++ php/include/bottom.php
@@ -0,0 +1,3 @@
+<div class="copyright">
+    Copyright ⓒ thfql4136. Allright Reserved.
+</div>
---
+++ php/main/main.php
@@ -0,0 +1,12 @@
+<? include_once "../include/head.php"; ?>
+<? include_once "../include/top.php"; ?>
+<? include_once "../include/nav.php"; ?>
+<!--본문 영영.시작-->
+<div class="mbody">
+
+메인페이지
+
+</div>
+<!--본문 영영.종료-->
+<? include_once "../include/bottom.php"; ?>
+<? include_once "../include/foot.php"; ?>
